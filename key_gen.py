d1625a98c4b39dd4c47c21cf5896949019ccb94a
petat-irrumator <95502484+petat-irrumator@users.noreply.github.com>
2022-02-09 19:58:17 +0500

    Update key_gen.py

--- key_gen.py
+++ key_gen.py
@@ -7,7 +7,7 @@ import secrets
 def make_random_password():
     # list that contains all the keywords from which the password would be randomly generated from
     password_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-                     'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'password_list', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '`', '~', '?', '/', '|', '<', '>', '.', ',', ';', ':']
+                     'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',  'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '`', '~', '?', '/', '|', '<', '>', '.', ',', ';', ':']
 
     # making the random 20 digit password
     key_1 = secrets.choice(password_list)
